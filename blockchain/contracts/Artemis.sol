43071f92e6ea8054289675fe87ed980bd79ab25e
km <kidommoc@gmail.com>
2023-04-14 21:26:12 +0800

    contract: add events, publisher registry and some query functions

--- blockchain/contracts/Artemis.sol
+++ blockchain/contracts/Artemis.sol
@@ -4,10 +4,14 @@ pragma solidity ^0.8.13;
 import "./ArtemisMessage.sol";
 
 contract Artemis {
+    event UploadArticleEvent(string fileAddr, string title, string author, address authorAddr, uint256 date);
+    event RemoveArticleEvent(string fileAddr);
+
     struct Article {
         address authorAddr;
         string title;
         string author;
+        uint256 date;
         bool requireSubscribing;
     }
 
@@ -18,57 +22,77 @@ contract Artemis {
         string encryptedAESKey;
     }
 
-    struct DistributorInfo {
-        uint subscribePrice;
+    struct PublisherInfo {
+        string pubKey;
+        uint subscribingPrice;
         mapping(address subscriber => SubscribeInfo) subscribers;
     }
 
     ArtemisMessage internal _message;
-    mapping(address distributor => DistributorInfo) internal _dists;
+    mapping(address publisher => PublisherInfo) internal _publs;
     mapping(string fileAddr => Article) internal _articles;
 
     constructor(address msgAddr) {
         _message = ArtemisMessage(msgAddr);
     }
 
-    function setSubscribingPrice(uint price) public {
-        DistributorInfo storage dist = _dists[msg.sender];
-        dist.subscribePrice = price;
-    }
-    
-    function getSubscribingPrice(address dist) public view returns (uint) {
-        return _dists[dist].subscribePrice;
+    function registerPublisher(string calldata pubKey, uint price) public {
+        require(bytes(pubKey).length != 0, "empty public key!");
+        if (price < 0)
+            price = 0;
+        _publs[msg.sender].pubKey = pubKey;
+        _publs[msg.sender].subscribingPrice = price;
     }
 
-    function subscribe(address payable dist, uint months, string calldata pubKey) public payable {
-        require(dist != address(0), "empty distributor address!");
-        DistributorInfo storage distInfo = _dists[dist];
-        require(msg.value >= months * distInfo.subscribePrice, "insufficiently paying!");
-        SubscribeInfo storage subInfo = distInfo.subscribers[msg.sender];
+    function getPublisherPubKey(address publisher) public view returns (string memory pubKey) {
+        require(publisher != address(0), "empty publisher address!");
+        return _publs[publisher].pubKey;
+    }
+
+    function setSubscribingPrice(uint price) public {
+        if (price < 0)
+            price = 0;
+        _publs[msg.sender].subscribingPrice = price;
+    }
+    
+    function getSubscribingPrice(address publ) public view returns (uint) {
+        return _publs[publ].subscribingPrice;
+    }
+
+    function subscribe(address payable publ, uint months, string calldata pubKey) public payable {
+        require(publ != address(0), "empty publisher address!");
+        PublisherInfo storage publInfo = _publs[publ];
+        require(msg.value >= months * publInfo.subscribingPrice, "insufficiently paying!");
+        SubscribeInfo storage subInfo = publInfo.subscribers[msg.sender];
         // being subscribing now?
         if (subInfo.time == 0 || subInfo.time < block.timestamp) {
             // no
-            _message.sendMessage(dist, 1, pubKey); // 1 means SUB_REQ
+            _message.sendMessage(publ, 1, pubKey); // 1 means SUB_REQ
             subInfo.requestTime = block.timestamp + months*30 days + 1 days;
             subInfo.payment = msg.value;
         }
         else {
             // yes
             subInfo.time += months*30 days + 1 days;
-            dist.transfer(msg.value);
+            publ.transfer(msg.value);
         }
     }
 
     function admitSubscribing(address subscriber, string calldata encKey) public {
         require(subscriber != address(0), "empty subscriber address!");
         require(bytes(encKey).length != 0, "empty Encryted AES Key!");
-        SubscribeInfo storage info = _dists[msg.sender].subscribers[subscriber];
+        SubscribeInfo storage info = _publs[msg.sender].subscribers[subscriber];
         _message.sendMessage(subscriber, 2, "OK");
         info.encryptedAESKey = encKey;
         info.time = info.requestTime;
         payable(msg.sender).transfer(info.payment);
         delete info.requestTime;
         delete info.payment;
+    }
+
+    function getSubscribingTime(address publ) public view returns (uint time) {
+        require(publ != address(0), "empty publisher address!");
+        return _publs[publ].subscribers[msg.sender].time;
     }
 
     function uploadArticle(string calldata file, string calldata title, string calldata author, bool requireSubscribing) public {
@@ -79,13 +103,16 @@ contract Artemis {
         _articles[file].title = title;
         _articles[file].author = author;
         _articles[file].authorAddr = msg.sender;
+        _articles[file].date = block.timestamp;
         _articles[file].requireSubscribing = requireSubscribing;
+        emit UploadArticleEvent(file, title, author, msg.sender, block.timestamp);
     }
 
     function removeArticle(string calldata file) public {
         require(bytes(file).length != 0, "empty article location!");
         require(msg.sender == _articles[file].authorAddr, "not article owner!");
         delete _articles[file];
+        emit RemoveArticleEvent(file);
     }
 
     function getArticleInfo(string calldata file) public view
@@ -103,9 +130,9 @@ contract Artemis {
         require(a.authorAddr != address(0), "article doesn't exist!");
         if (!a.requireSubscribing)
             return (true, "");
-        DistributorInfo storage distInfo = _dists[a.authorAddr];
-        if (distInfo.subscribers[msg.sender].time < block.timestamp)
+        PublisherInfo storage publInfo = _publs[a.authorAddr];
+        if (publInfo.subscribers[msg.sender].time < block.timestamp)
             return (false, "");
-        return (true, distInfo.subscribers[msg.sender].encryptedAESKey);
+        return (true, publInfo.subscribers[msg.sender].encryptedAESKey);
     }
 }
